689f466e6f4b2ff5cf07add6823d8a6b11e22738
Markus Monz <markusmonz@googlemail.com>
2015-08-02 19:49:39 +0200

    added new state 'quit' between 'established' and 'listen

---
+++ src/ctcom/statesImpl/server/QuitServerState.java
@@ -0,0 +1,58 @@
+package ctcom.statesImpl.server;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import javax.naming.OperationNotSupportedException;
+
+import ctcom.CtcomServer;
+import ctcom.messageImpl.CtcomMessage;
+import ctcom.states.ServerState;
+
+public class QuitServerState implements ServerState {
+
+	@Override
+	public Socket accept(CtcomServer server) throws OperationNotSupportedException {
+		server.changeState(new ListenServerState());
+		Socket client = null;
+		try {
+			client = server.getServerSocket().accept();
+			server.setClientSocket(client);
+		} catch (IOException e) {
+			e.printStackTrace();
+			server.changeState(new ClosedServerState());
+		}
+		return client;
+	}
+
+	@Override
+	public void close(CtcomServer server) throws OperationNotSupportedException {
+		try {
+			server.getServerSocket().close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		server.changeState(new ClosedServerState());
+	}
+
+	@Override
+	public void sendConnectionAck(CtcomServer server, CtcomMessage message) throws OperationNotSupportedException {
+		throw new OperationNotSupportedException("Faild to send connection acknowledge, ctcom connection is closed.");
+	}
+
+	@Override
+	public CtcomMessage getConnectRequest(CtcomServer server, Socket client) throws OperationNotSupportedException {
+		throw new OperationNotSupportedException("Failed to serve client, ctcom connection is closed.");
+	}
+
+	@Override
+	public CtcomMessage readData(CtcomServer server) throws OperationNotSupportedException {
+		throw new OperationNotSupportedException("Cannot read data, ctcom connection is closed.");
+	}
+
+	@Override
+	public void quit(CtcomServer server, String message) throws OperationNotSupportedException {
+		throw new OperationNotSupportedException("Faild to quit ctcom connection, connection is closed.");
+	}
+
+}
